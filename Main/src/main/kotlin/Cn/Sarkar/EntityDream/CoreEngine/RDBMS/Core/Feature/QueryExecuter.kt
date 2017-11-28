068de0c94b559002890171928a638356fb4325ff
yeganaaa <yeganaaa@163.com>
2017-11-28 15:27:08 +0600

    سانلىق مەلۇمات قۇرۇلمىسى ئەلالاشتۇرۇپ تولۇقلاندى، فۇنكىسىيە قوللاش ئىقتىدارى قوشۇلدى ئىزدەشكە قارىتا يىڭى فۇنكىسىيەلەر قوشۇلدى PipeLine قوللىشى قوشۇلدى ئومومى رامكا قۇرۇلمىسىنىڭ چۈشەندۈرۈش دوكۇمىنتى قوشۇلدى، 27.چىسلادىكى ئۇششاق چۈششەك كودلار

--- Main/src/main/kotlin/Cn/Sarkar/EntityDream/CoreEngine/RDBMS/Core/Feature/QueryExecuter.kt
+++ Main/src/main/kotlin/Cn/Sarkar/EntityDream/CoreEngine/RDBMS/Core/Feature/QueryExecuter.kt
@@ -1,12 +1,15 @@
 package Cn.Sarkar.EntityDream.CoreEngine.RDBMS.Core.Feature
 
 import Cn.Sarkar.EntityDream.CoreEngine.RDBMS.Core.PipeLine.CorePipeLine
+import Cn.Sarkar.EntityDream.CoreEngine.RDBMS.Core.PipeLine.IPipeLineSubject
+import Cn.Sarkar.EntityDream.CoreEngine.RDBMS.Core.PipeLine.QueryExecutionSubject
+import Cn.Sarkar.EntityDream.CoreEngine.RDBMS.IDataContext
 import Cn.Sarkar.EntityDream.Pipeline.Core.Info.FeatureInfo
 import Cn.Sarkar.EntityDream.Pipeline.Core.PipeLineContext
 import Cn.Sarkar.EntityDream.Pipeline.Core.PipeLineFeature
 import Cn.Sarkar.EntityDream.Pipeline.Core.PipeLineFeatureMetaData
 
-object QueryExecuter : PipeLineFeature<CorePipeLine, Unit, Unit>() {
+object QueryExecuter : PipeLineFeature<IPipeLineSubject, IDataContext, Unit>() {
     override val getMetaData: PipeLineFeatureMetaData = PipeLineFeatureMetaData(CorePipeLine.process, "Cn.Sarkar.EntityDreams.Core.QueryExecute")
     override val info: FeatureInfo = FeatureInfo(
             "Query Execute",
@@ -17,7 +20,10 @@ object QueryExecuter : PipeLineFeature<CorePipeLine, Unit, Unit>() {
             "v0.1"
     )
 
-    override fun PipeLineContext<CorePipeLine, Unit>.onExecute(subject: CorePipeLine) {
-
+    override fun PipeLineContext<IPipeLineSubject, IDataContext>.onExecute(subject: IPipeLineSubject) {
+        if (subject is QueryExecutionSubject)
+        {
+            subject.
+        }
     }
 }
--- Main/src/main/kotlin/Cn/Sarkar/EntityDream/CoreEngine/RDBMS/Core/PipeLine/QueryExecutionSubject.kt
+++ Main/src/main/kotlin/Cn/Sarkar/EntityDream/CoreEngine/RDBMS/Core/PipeLine/QueryExecutionSubject.kt
@@ -8,13 +8,15 @@ Time: 4:05 PM
 
 package Cn.Sarkar.EntityDream.CoreEngine.RDBMS.Core.PipeLine
 
+import java.sql.ResultSet
+
 interface ExecutionResult
 
 data class EffectedRows(var Count: Int) : ExecutionResult
-//data class
+data class RedirectedResult(var result: ResultSet)
 
 
-class QueryExecutionSubject(var result: ExecutionResult) : IPipeLineSubject{
+class QueryExecutionSubject(var result: ExecutionResult? = null) : IPipeLineSubject{
     override val operationName: String = "Execute SQL Query"
     override val operationDescription: String = "This is sql query execution operation"
     var groups: HashMap<String, QueryGroup> = HashMap()
